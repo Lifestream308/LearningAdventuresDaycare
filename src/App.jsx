c77c0a21f280e2a881143df41236dfb5d9739f10
Christian Arroyo <ChristianArroyo308@yahoo.com>
2024-08-24 01:03:09 -0700

    trying React.lazy and Suspense tag in App.jsx file with react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-// import { useState } from 'react'
+import React, {Suspense} from 'react'
 import { Route, Routes } from 'react-router-dom'
-import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
 import MainSectionsComponent from './components/MainSectionsComponent'
-import AboutComponent from './components/AboutComponent'
-import ContactComponent from './components/ContactComponent'
+
+const AboutComponent = React.lazy(() => import('./components/AboutComponent'))
+const ContactComponent = React.lazy(() => import('./components/ContactComponent'))
+const FooterComponent = React.lazy(() => import('./components/FooterComponent'))
+// import AboutComponent from './components/AboutComponent'
+// import ContactComponent from './components/ContactComponent'
+// import FooterComponent from './components/FooterComponent'
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -13,13 +17,17 @@ function App() {
     <>
       <HeaderComponent />
 
-      <Routes>
-        <Route path='/' element={ <MainSectionsComponent /> } />
-        <Route path='/about' element={ <AboutComponent /> } />
-        <Route path='/contact' element={ <ContactComponent /> } />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path='/' element={ <MainSectionsComponent /> } />
+          <Route path='/about' element={ <AboutComponent /> } />
+          <Route path='/contact' element={ <ContactComponent /> } />
+        </Routes>
+      </Suspense>
 
-      <FooterComponent />
+      <Suspense fallback={<div>Loading...</div>}>
+        <FooterComponent />
+      </Suspense>
     </>
   )
 }
