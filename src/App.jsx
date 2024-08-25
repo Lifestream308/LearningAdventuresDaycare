cf94100955f7b49d6b68d73f8d62ea4521e68dbe
Christian Arroyo <ChristianArroyo308@yahoo.com>
2024-08-25 00:50:50 -0700

    removed suspense and lazy load from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
-import React, {Suspense} from 'react'
+// import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import HeaderComponent from './components/HeaderComponent'
 import MainSectionsComponent from './components/MainSectionsComponent'
 
-const AboutComponent = React.lazy(() => import('./components/AboutComponent'))
-const ContactComponent = React.lazy(() => import('./components/ContactComponent'))
-const FooterComponent = React.lazy(() => import('./components/FooterComponent'))
-// import AboutComponent from './components/AboutComponent'
-// import ContactComponent from './components/ContactComponent'
-// import FooterComponent from './components/FooterComponent'
+// const AboutComponent = React.lazy(() => import('./components/AboutComponent'))
+// const ContactComponent = React.lazy(() => import('./components/ContactComponent'))
+// const FooterComponent = React.lazy(() => import('./components/FooterComponent'))
+import AboutComponent from './components/AboutComponent'
+import ContactComponent from './components/ContactComponent'
+import FooterComponent from './components/FooterComponent'
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <>
       <HeaderComponent />
 
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path='/' element={ <MainSectionsComponent /> } />
-          <Route path='/about' element={ <AboutComponent /> } />
-          <Route path='/contact' element={ <ContactComponent /> } />
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route path='/' element={ <MainSectionsComponent /> } />
+        <Route path='/about' element={ <AboutComponent /> } />
+        <Route path='/contact' element={ <ContactComponent /> } />
+      </Routes>
 
-      <Suspense fallback={<div>Loading...</div>}>
-        <FooterComponent />
-      </Suspense>
+      <FooterComponent />
     </>
   )
 }
