888d2ce99809d50c2e18164cc92b42f056f75ff7
Christian <ChristianArroyo308@yahoo.com>
2024-10-24 17:04:22 -0700

    adjusting link to site map in footer

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function FooterComponent() {
   return (
     <>
-        <footer className='flex flex-col mt-14 mb-16 gap-8 text-blue-900 text-center'>
+        <footer className='flex flex-col mt-14 mb-16 gap-6 text-blue-900 text-center'>
             <p className='break-all'>LearningTimeAdventures@gmail.com</p>
-            <Link to='/sitemap' className='px-4 py-2 text-xl font-light text-blue-800 hover:underline'>Site Map</Link>
             <small>©{new Date().getFullYear()} by Learning Time Adventures Daycare</small>
+            <Link to='/sitemap' className='px-4 py-2 text-base font-light text-blue-800 underline hover:text-blue-950'>Site Map</Link>
         </footer>
     </>
   )
